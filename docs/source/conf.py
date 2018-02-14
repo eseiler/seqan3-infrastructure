7d8709c039a1420ffec1d1e376388452efbfaef8
Enrico Seiler <eseiler@users.noreply.github.com>
2018-02-14 20:35:31 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -114,8 +114,8 @@ def generateRST(outDir, moduleName, listModules, listFiles) :
     for fileName in listFiles :
         print(outDir)
         print(fileName)
-        print(".. doxygenfile:: %s\n" % (outDir+'/'+fileName))
-        outFile.write(".. doxygenfile:: %s\n" % (outDir+'/'+fileName))
+        print(".. doxygenfile:: %s\n" % ('/'.join(outDir.split('/')[2:])+'/'+fileName))
+        outFile.write(".. doxygenfile:: %s\n" % ('/'.join(outDir.split('/')[2:])+'/'+fileName))
         outFile.write("   :project: Seqan3\n\n") # TODO generic
 
     # toctree
